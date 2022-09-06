94e9ce42a98d2969f7fd05558d019f01164d021b
Treboder <robert.kohn@gmx.net>
2022-09-06 09:34:26 +0200

    Add new endpoint /coord making MapsController take multiple parameters (i.e. the coordinates in this case)

--- P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
+++ P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
@@ -1,16 +1,36 @@
 package com.udacity.boogle.maps;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/maps")
 public class MapsController {
 
-    @GetMapping
-    public Address get(@RequestParam Double lat, @RequestParam Double lon) {
-        return MockAddressRepository.getRandom();
+    @GetMapping("/random")
+    public ResponseEntity<Address> get() {
+        return new ResponseEntity<Address>(MockAddressRepository.getRandom(), HttpStatus.OK);
     }
+
+    // with lat=5 and lon=1 the call would be http://localhost:9191/maps/coord?lat=5&lon=1
+    // cf. https://www.baeldung.com/spring-request-param
+    @GetMapping("/coord")
+    public ResponseEntity<Address> get(@RequestParam Map<String, String> customQuery) {
+        //System.out.println("customQuery = lat " + customQuery.get("lat"));
+        //System.out.println("customQuery = lon " + customQuery.get("lon"));
+        return new ResponseEntity<Address>(MockAddressRepository.getRandom(), HttpStatus.OK);
+    }
+
+//    The original function definiton from the starter project
+//    @GetMapping("/coord/")
+//    public ResponseEntity<Address> get(@RequestParam Double lat, @RequestParam Double lon) {
+//        return new ResponseEntity<Address>(MockAddressRepository.getRandom(), HttpStatus.OK);
+//    }
+
 }
