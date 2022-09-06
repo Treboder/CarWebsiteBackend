302dfa4148fa543345f458bb901039ba4c1713cd
Treboder <robert.kohn@gmx.net>
2022-09-06 14:02:11 +0200

    Refactor MapsController.java

--- P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
+++ P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
@@ -13,24 +13,11 @@ import java.util.Map;
 @RequestMapping("/maps")
 public class MapsController {
 
-    @GetMapping("/random")
-    public ResponseEntity<Address> get() {
-        return new ResponseEntity<Address>(MockAddressRepository.getRandom(), HttpStatus.OK);
-    }
-
     // with lat=1 and lon=2 the call would be http://localhost:9191/maps/coord?lat=1&lon=2
-    // cf. https://www.baeldung.com/spring-request-param
-    @GetMapping("/coord")
-    public ResponseEntity<Address> get(@RequestParam Map<String, String> customQuery) {
-        System.out.println("customQuery = lat " + customQuery.get("lat"));
-        System.out.println("customQuery = lon " + customQuery.get("lon"));
+    // cf. https://www.baeldung.com/spring-request-param for more examples
+    @GetMapping
+    public ResponseEntity<Address> get(@RequestParam Double lat, @RequestParam Double lon) {
         return new ResponseEntity<Address>(MockAddressRepository.getRandom(), HttpStatus.OK);
     }
-
-//    The original function definition from the starter project
-//    @GetMapping("/coord/")
-//    public ResponseEntity<Address> get(@RequestParam Double lat, @RequestParam Double lon) {
-//        return new ResponseEntity<Address>(MockAddressRepository.getRandom(), HttpStatus.OK);
-//    }
 
 }
