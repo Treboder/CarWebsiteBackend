20d17a2eb72db28ca49c11001a7d3fdc87a50360
Treboder <robert.kohn@gmx.net>
2022-09-07 17:12:44 +0200

    Enable PricingServiceWithHeurekaApplication to register its service to a running eureka instance

--- pricing-service-with-eureka/src/main/java/com/udacity/pricing/PricingServiceWithHeurekaApplication.java
+++ pricing-service-with-eureka/src/main/java/com/udacity/pricing/PricingServiceWithHeurekaApplication.java
@@ -2,12 +2,14 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Add proper test for the microservice.
  */
 @SpringBootApplication
+@EnableEurekaClient // is optional when spring-cloud-starter-netflix-eureka-client is part of the dependencies
 public class PricingServiceWithHeurekaApplication {
 
     public static void main(String[] args) {
